d23023edf95b896f66d82d72614faff23727146b
Hanan-Alkahwaji <hanan.alkahwaji2003.8@gmail.com>
2024-10-22 16:11:49 +0300

    change setting for gh-page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,18 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+
+
+
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
+
+reportWebVitals();
